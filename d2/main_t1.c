7cb2d3c0d286d9c8b087718971fc0a55269b81bf
miikaoskari <miika.karjalainen13@gmail.com>
2024-12-03 21:01:22 +0200

    add passing day 1 task 1

---
+++ d2/main_t1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 40
+#define MAX_NUMBERS 20
+
+int main()
+{
+    FILE *fp;
+
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL)
+    {
+        perror("file open error");
+        return 1;
+    }
+
+    /* The levels are either all increasing or all decreasing. */
+    /* Any two adjacent levels differ by at least one and at most three. */
+    /* Tolerate single bad level */
+    char line[MAX_LINE_LENGTH];
+    int numbers[MAX_NUMBERS];
+    int safe_functions = 0;
+
+    while (fgets(line, sizeof(line), fp))
+    {
+        char *token = strtok(line, " ");
+        char *endptr;
+        int count = 0;
+
+        while (token != NULL)
+        {
+            numbers[count] = strtol(token, &endptr, 10);
+            count++;
+            token = strtok(NULL, " \n");
+        }
+
+        int increasing = 1;
+        int decreasing = 1;
+        int valid_distance = 1;
+
+        /* check if monotonic */
+        for (int i = 0; i < count - 1; i++)
+        {
+            if (numbers[i] < numbers[i + 1])
+            {
+                increasing = 0;
+            } 
+            else if (numbers[i] > numbers[i + 1])
+            {
+                decreasing = 0;
+            }
+
+            /* distance between two numbers */
+            int diff = abs(numbers[i] - numbers[i + 1]);
+
+            /* check if the difference is between 1 and 3 */
+            if (diff < 1 || diff > 3)
+            {
+                valid_distance = 0;
+                break;   
+            }
+        }
+        if (valid_distance && (increasing || decreasing))
+        {
+            safe_functions++;
+        }
+
+    }
+
+    printf("Safe functions: %d\n", safe_functions);
+}
